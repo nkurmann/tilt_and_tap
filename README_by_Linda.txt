33ecfef9b281add96762bc76ee7894b1ad9c6c12
nmk <kurmannn@ethz.ch>
2015-04-21 13:58:57 +0200

    selector is moving

---
+++ README_by_Linda.txt
@@ -0,0 +1,46 @@
+========================HOW TO TEST YOUR EXERCISE SOLUTION ON YOUR MOBILE DEVICE================
+
+In this tutorial we will see how to run your web application, stored on your laptop/desktop computer on your mobile device.
+
+
+
+1) Download XAMPP (https://www.apachefriends.org/index.html)
+
+2) Install XAMPP
+
+3) Put the folder that contains your solution in the htdocs directory of XAMPP (it is usually stored in the following path: C:\xampp\htdocs)
+
+4) Run Apache by using the XAMPP control panel
+
+5) You should now check for your IP address.
+On Windows computers you can do it by going:
+
+	Control Panel > Network and Internet > Network Connections > Local Area Connection
+    
+Press the "details" button and read the IPv4 number, that is your IP address.
+
+
+6) Open a browser on your mobile device (better if Chrome)
+
+   Make sure that your mobile device and your computer (where your solution is stores in the xampp directory) are connected to the same wifi or the same LAN network
+
+7) Write down the following URL 
+
+
+YourIpAddreess:Port\YourFolderName
+
+
+
+8) Have fun!
+
+
+
+========================HOW TO DEBUG YOUR SOLUTION ON YOUR MOBILE================================
+
+
+If you want to debug your solution using your phone, here you can find a tutorial on how to test your application on an android device.
+
+https://developer.chrome.com/devtools/docs/remote-debugging
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 $(document).ready( function() {
     var thumbs_block = $('#thumbs_block');
-    alert("thumbs up");
+    //alert("thumbs up");
 
     function nav_left () {
-        alert("nav_left");
-        var prev_sib = $(".selected").previousSibling;
-        if (prev_sib != null) {
+        //alert("nav_left");
+        var prev_img = $(".selected").parent().prev().children("img");
+        if (prev_img.length) { //check existence
             $(".selected").removeClass("selected").addClass("notselected");
-            prev_sib.removeClass("notselected").addClass("selected");
+            prev_img.removeClass("notselected").addClass("selected");
+        }
+        else {
+            $("body").auditory_feedback({ audio_file: "sound/test.mp3" });
         }
     };
 
     function nav_right () {
-        alert("nav_right");
-        var next_sib = $(".selected").nextSibling;
-        if (next_sib != null) {
+        //alert("nav_right")
+        var next_img = $(".selected").parent().next().children("img");
+        //alert(next_img);
+        //next_img.attr("src","img/img1.jpg");
+        
+        if (next_img.length) { //check existence
             $(".selected").removeClass("selected").addClass("notselected");
-            next_sib.removeClass("notselected").addClass("selected");
+            next_img.removeClass("notselected").addClass("selected");
         }
+       
     };
 
 
@@ -26,8 +33,17 @@ $(document).ready( function() {
 
     $("#thumbs_block").tiltandtap({
         onTiltLeft  : nav_left,
-        onTiltRight    : nav_right
+        onTiltRight : nav_right, 
+        onTiltUp    : changeColor
     });
+    
+    
+    function changeColor () {
+        var rcolor=Math.floor((Math.random() * 250) + 1);
+        var gcolor=Math.floor((Math.random() * 250) + 1);
+        var bcolor=Math.floor((Math.random() * 250) + 1);
+        $("#thumbs_block").css("background-color", "rgb(" + rcolor + "," + gcolor + "," + bcolor + ")");
+    }
 });
 
     
